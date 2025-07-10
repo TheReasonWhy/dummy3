305d5b81f1336d0419abeb1863bb18fd5ba2c445
i.pestriakov <i.pestriakov@vk.team>
2025-07-10 10:42:15 +0300

    добавил поддержку flatpak

--- Main.qml
+++ Main.qml
@@ -4,5 +4,5 @@ Window {
     width: 640
     height: 480
     visible: true
-    title: qsTr("I'm dummy #3")
+    title: qsTr("I'm dummy #9")
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/dummy3/Main.qml"));
+    const QUrl url(QStringLiteral("qrc:/dummy9/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
